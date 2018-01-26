e2fd506eb5661c573ef81a12245e0c284735942b
David Dassau <todieahero@hotmail.com>
2018-01-26 11:41:30 -0600

    completed sales and marketing departments

---
+++ BangazonDepartment/classes/Department.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BangazonDepartment.classes
+{
+    class Department
+    {
+
+        public string Name { get; set; }
+        public string Supervisor { get; set; }
+        public double Budget { get; set; }
+        public int Employee_Count { get; set; }
+
+        public virtual void Department_Description()
+        {
+            Console.WriteLine($"{Supervisor} is the supervisor of the {Name} department, and has {Employee_Count} employees.");
+        }
+
+        public virtual void SetBudget(double budget)
+        {
+            Budget = budget;
+            Console.WriteLine($"{Budget} is the budget for this department");
+        }
+
+    }
+}
---
+++ BangazonDepartment/classes/Sales.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BangazonDepartment.classes
+{
+    class Sales : Department
+    {
+
+        public int SalesCompleted { get; set; }
+
+        public void SalesGoals()
+        {
+            Console.WriteLine($"The sales department has made {SalesCompleted} sales so far this year.");
+        }
+
+        public override void Department_Description()
+        {
+            SalesCompleted += 30;
+            base.Department_Description();
+        }
+
+        public override void SetBudget(double budget)
+        {
+            Budget = budget + 30000;
+            Console.WriteLine($"This is the budget for the sales department: ${Budget}");
+        }
+
+    }
+}
